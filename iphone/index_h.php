e951a15997a2781076858e2e6188fbca946feb9e
starcraft-j <jang@starcraft-n.co.jp>
2022-02-09 10:45:11 +0900

    防風通聖散ページ前に戻す

--- iphone/index_h.php
+++ iphone/index_h.php
@@ -108,17 +108,8 @@
 }
 </style>
 <section id="special">
-    <!-- 美活ニュース参考 -->
-    
   
-    <div class="last-item item1">
-        <p>テイシ防風通聖散</span> … <span class="bold red">おなかに皮下脂肪が多く、便秘がちな方</span>の<span class="bold red bg-y">肥満症に効く漢方薬！</span></p>
-        <p class="right">
-        <a target="_blank" href="https://bi-navi.com/links/teishi.php<?php echo "?" . $query2; ?>">→ テイシ防風通聖散を<br />
-            お得に買える公式サイト
-            </a>
-        </p>
-    </div>
+   
     <div class="last-item item1">
         <p><span class="bold">生漢煎（しょうかんせん）</span> … <span class="bold red bg-y">お腹の脂肪を分解・燃焼・排出促進！</span>効能効果が認められた<span class="red bold">満量処方の漢方薬！</span></p>
         <p class="right">
@@ -127,10 +118,20 @@
             </a>
         </p>
     </div>
+    
     <div class="last-item item1">
         <p><span class="bold">EGライフ</span> … <span class="red bold">肥満症</span>に効能効果を認められた、<span class="bg-y red bold">脂肪を落とす防風通聖散！</span></p>
         <p class="right">
         <a target="_blank" href="https://bi-navi.com/links/eg.php<?php echo "?" . $query2; ?>">→ EGライフを<br />
+            お得に買える公式サイト
+            </a>
+        </p>
+    </div>
+
+    <div class="last-item item1">
+        <p>テイシ防風通聖散</span> … <span class="bold red">おなかに皮下脂肪が多く、便秘がちな方</span>の<span class="bold red bg-y">肥満症に効く漢方薬！</span></p>
+        <p class="right">
+        <a target="_blank" href="https://bi-navi.com/links/teishi.php<?php echo "?" . $query2; ?>">→ テイシ防風通聖散を<br />
             お得に買える公式サイト
             </a>
         </p>
